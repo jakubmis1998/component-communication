a640b588010c3e099a561e9f9908a1b976895940
jakub.mis <jakub.mis@mga.com.pl>
2020-07-15 14:51:05 +0200

    Added bages

--- subject-app/src/app/app.module.ts
+++ subject-app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 
 import { ToastrModule } from 'ngx-toastr';
 import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { MatButtonModule } from '@angular/material/button';
       positionClass: 'toast-bottom-center',
       preventDuplicates: true,
     }),
-    MatButtonModule
+    MatButtonModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
